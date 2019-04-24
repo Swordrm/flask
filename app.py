e0d5b5f10fcae11c2e60089d6b1a2a01ec232ea4
Swordrm <1738769499@qq.com>
2019-04-24 20:39:02 +0800

    添加name路由

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/<name>')
+def user(name):
+    return '<h1>Hello, %s!</h1>' % name
+
 
 if __name__ == '__main__':
     app.run()
